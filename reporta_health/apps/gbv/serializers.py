fc3d5b7722a1fd4d1ee9a8dcdc052ef02e0e4dc0
khalmam <genkhal25@gmail.com>
2026-06-07 13:50:29 +0100

    feat: update GBV serializers

--- reporta_health/apps/gbv/serializers.py
+++ reporta_health/apps/gbv/serializers.py
@@ -2,12 +2,15 @@
 Serializers for GBV app.
 """
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_field
+from drf_spectacular.types import OpenApiTypes
 from .models import GBVService
 
 
 class GBVServiceSerializer(serializers.ModelSerializer):
 
     distance = serializers.SerializerMethodField()
+    location = serializers.SerializerMethodField()
 
     class Meta:
         model  = GBVService
@@ -29,18 +32,30 @@ class GBVServiceSerializer(serializers.ModelSerializer):
             'distance',
         )
 
-    def get_distance(self, obj):
+    @extend_schema_field(OpenApiTypes.FLOAT)
+    def get_distance(self, obj) -> float:
         if hasattr(obj, 'distance'):
             return round(obj.distance.m, 1)
+        return None
+
+    @extend_schema_field({'type': 'object', 'properties': {
+        'latitude':  {'type': 'number'},
+        'longitude': {'type': 'number'},
+    }})
+    def get_location(self, obj):
+        if obj.location:
+            return {
+                'latitude':  obj.location.y,
+                'longitude': obj.location.x,
+            }
         return None
 
 
 class GBVNearbyResultSerializer(serializers.Serializer):
     """
     Combined serializer for the /gbv/nearby/ endpoint.
-    Returns both GBV services and SARC facilities in one list.
     """
-    kind          = serializers.CharField()   # 'gbv_service' or 'sarc'
+    kind          = serializers.CharField()
     id            = serializers.IntegerField()
     name          = serializers.CharField()
     state         = serializers.CharField()
@@ -50,4 +65,4 @@ class GBVNearbyResultSerializer(serializers.Serializer):
     services      = serializers.CharField()
     distance_m    = serializers.FloatField()
     location      = serializers.DictField()
-    extra         = serializers.DictField()   # kind-specific fields
+    extra         = serializers.DictField()
